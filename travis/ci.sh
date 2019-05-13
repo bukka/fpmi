87016ebd6df18440fbdf6af7e2e7bab8966509fb
Jakub Zelenka <bukka@php.net>
2019-05-13 18:44:31 +0100

    Enable opcache in Travis

--- travis/ci.sh
+++ travis/ci.sh
@@ -4,5 +4,5 @@ cd php-src/sapi
 git clone --depth=5 --branch=master https://github.com/bukka/fpmi.git
 cd ../
 ./buildconf --force
-./configure --disable-all --enable-fpmi
+./configure --disable-all --enable-opcache --enable-fpmi
 make
